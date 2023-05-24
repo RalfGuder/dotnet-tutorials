471f97b39f93b8016b1a3e0bc3598157037598d8
RalfGuder <ralfguder@t-online.de>
2023-05-24 11:22:02 +0200

    Add Files

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    // Tutorial 1
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
