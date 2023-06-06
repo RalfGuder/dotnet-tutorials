be6779c476ca7b3ede1a6d80a71053636653d74c
RalfGuder <45797275+RalfGuder@users.noreply.github.com>
2023-06-06 12:44:35 +0200

    Update Program.cs

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -13,7 +13,7 @@ string[] pettingZoo =
 RandomizeAnimals();
 
 // Erstellen Sie als Nächstes eine Pseudocodemethode, um die Tiergruppen zuzuweisen.
-// string[,] group = AssignGroup();
+string[,] group = AssignGroup();
 
 // Sie können den Schulnamen ganz einfach mit Console.Write ausgeben.
 Console.WriteLine("School A");
@@ -33,4 +33,20 @@ void RandomizeAnimals()
         pettingZoo[r] = pettingZoo[i];
         pettingZoo[i] = temp;
     }
+}
+
+string[,] AssignGroup(int groups = 6) 
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
 }
