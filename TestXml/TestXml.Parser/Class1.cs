c284ef03c0e5978d852d956f4639225f120d4cca
RalfGuder <ralfguder@users.noreply.github.com>
2023-06-08 17:07:07 +0200

    Parser

---
+++ TestXml/TestXml.Parser/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Gudchensoft.TestXml.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Gudchensoft.TestXml.Parser
+{
+	public class XmlParser : IXmlParser
+	{
+		string IXmlParser.Tagname 
+		{
+			get { return ""; }
+		}
+
+		void IXmlParser.Parse(XmlReader reader)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
